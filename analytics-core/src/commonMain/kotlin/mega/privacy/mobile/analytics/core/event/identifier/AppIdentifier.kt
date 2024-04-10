11b48ac332014c68880b50257b14355952959919
Kevin Gozali <kg@mega.co.nz>
2024-04-10 14:00:43 +1200

    VPN-1281 AND - Add AppIdProvider and AppIdentifier

---
+++ analytics-core/src/commonMain/kotlin/mega/privacy/mobile/analytics/core/event/identifier/AppIdentifier.kt
@@ -0,0 +1,9 @@
+package mega.privacy.mobile.analytics.core.event.identifier
+
+data class AppIdentifier(val id: Int) {
+    init {
+        require(id in 1 .. 9) { "App identifier must be in 0 to 9" }
+    }
+
+    val identifier = id * 10_000
+}
---
+++ analytics-core/src/commonTest/kotlin/mega/privacy/mobile/analytics/core/event/identifier/AppIdentifierTest.kt
@@ -0,0 +1,30 @@
+package mega.privacy.mobile.analytics.core.mapper.mega.privacy.mobile.analytics.core.event.identifier
+
+import mega.privacy.mobile.analytics.core.event.identifier.AppIdentifier
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class AppIdentifierTest {
+    private lateinit var underTest: AppIdentifier
+
+    @Test
+    fun `test that app identifier throws an exception when id is more than 9`() {
+        assertFailsWith<IllegalArgumentException> {
+            underTest = AppIdentifier(10)
+        }
+    }
+
+    @Test
+    fun `test that app identifier throws an exception when id is less than 0`() {
+        assertFailsWith<IllegalArgumentException> {
+            underTest = AppIdentifier(-1)
+        }
+    }
+
+    @Test
+    fun `test that app identifier returns the correct identifier when in range`() {
+        underTest = AppIdentifier(5)
+        assertEquals(50_000, underTest.identifier)
+    }
+}
