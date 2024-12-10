2f9313779d67ca7592d52c55e3c059cc59ef1bcf
Tomasz Kuźma <tku@mega.co.nz>
2024-12-10 11:19:00 +0100

    MEET-4595: Port Legacy Meetings analytics events to KMM

--- shared/src/commonMain/kotlin/mega/privacy/mobile/analytics/event/LegacyEvents.kt
+++ shared/src/commonMain/kotlin/mega/privacy/mobile/analytics/event/LegacyEvents.kt
@@ -1,2 +1,20 @@
 package mega.privacy.mobile.analytics.event
+import mega.privacy.mobile.analytics.annotations.LegacyEvent
 
+@LegacyEvent(eventId = 99309)
+interface EndCallForAll
+
+@LegacyEvent(eventId = 99311)
+interface EndCallInNoParticipantsPopup
+
+@LegacyEvent(eventId = 99313)
+interface StayOnCallInNoParticipantsPopup
+
+@LegacyEvent(eventId = 99312)
+interface EnableCallSoundNotifications
+
+@LegacyEvent(eventId = 99314)
+interface DisableCallSoundNotifications
+
+@LegacyEvent(eventId = 99315)
+interface EndCallWhenEmptyCallTimeout
