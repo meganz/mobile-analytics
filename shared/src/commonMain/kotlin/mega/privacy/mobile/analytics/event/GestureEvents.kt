efd3ca830548aeb40f22ab27b68984dd34eac12f
Kevin Sun <ks@mega.co.nz>
2026-08-21 11:45:54 +0800

    AND-24628 Add analytics events for video player gesture interactions

--- shared/src/commonMain/kotlin/mega/privacy/mobile/analytics/event/GestureEvents.kt
+++ shared/src/commonMain/kotlin/mega/privacy/mobile/analytics/event/GestureEvents.kt
@@ -19,3 +19,27 @@ interface ActiveTransferDragAndDropToChangePriority
 
 @GestureEvent(gesture = "Drag to select started", screen = "Media Screen")
 interface MediaScreenDragToSelectStarted
+
+@GestureEvent(gesture = "Vertical swipe to adjust brightness", screen = "Video Player Screen")
+interface VideoPlayerBrightnessSwipe
+
+@GestureEvent(gesture = "Vertical swipe to adjust volume", screen = "Video Player Screen")
+interface VideoPlayerVolumeSwipe
+
+@GestureEvent(gesture = "Long press for 2x playback speed", screen = "Video Player Screen")
+interface VideoPlayerLongPressSpeed
+
+@GestureEvent(gesture = "Double tap to seek forward", screen = "Video Player Screen")
+interface VideoPlayerDoubleTapSeekForward
+
+@GestureEvent(gesture = "Double tap to seek backward", screen = "Video Player Screen")
+interface VideoPlayerDoubleTapSeekBackward
+
+@GestureEvent(gesture = "Zoom to fill screen", screen = "Video Player Screen")
+interface VideoPlayerZoomToFill
+
+@GestureEvent(gesture = "Zoom to fit screen", screen = "Video Player Screen")
+interface VideoPlayerZoomToFit
+
+@GestureEvent(gesture = "Pinch to zoom", screen = "Video Player Screen")
+interface VideoPlayerPinchToZoom
